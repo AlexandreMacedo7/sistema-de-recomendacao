5299e11002d58e7b296735beccacd77a8be53ee3
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-07-08 15:22:37 -0400

    Anotações de validações

--- src/main/java/com/macedo/livrorecomendacao/entity/Aluno.java
+++ src/main/java/com/macedo/livrorecomendacao/entity/Aluno.java
@@ -2,23 +2,34 @@ package com.macedo.livrorecomendacao.entity;
 
 
 import com.macedo.livrorecomendacao.enums.Turno;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Entity
 public class Aluno {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotBlank(message = "Nome de aluno não pode ser vazio.")
     private String nome;
+    @NotBlank(message = "Matricula não pode ser vazia.")
     private String matricula;
+    @NotBlank(message = "Turma não pode ser vazia.")
     private String turma;
 
     @Enumerated(EnumType.STRING)
     private Turno turno;
+    @OneToMany
+    private List<Avaliacao> avaliacaoLista = new ArrayList<>();
 }
 
