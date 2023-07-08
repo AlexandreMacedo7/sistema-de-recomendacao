c0b5a5c9a11c50ca6b8961e95d8321ae9cf84894
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-07-08 15:22:05 -0400

    Criação de entidade Avaliacao

---
+++ src/main/java/com/macedo/livrorecomendacao/entity/Avaliacao.java
@@ -0,0 +1,27 @@
+package com.macedo.livrorecomendacao.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+public class Avaliacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "aluno_id")
+    @NotNull
+    private Aluno aluno;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "livro_id")
+    @NotNull
+    private Livro livro;
+}
