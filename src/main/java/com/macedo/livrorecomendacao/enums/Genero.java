f9784b4401f79760b8da6217fd8e99092b916447
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-07-08 15:20:18 -0400

    Criação de enum Genero para uso de Livro

---
+++ src/main/java/com/macedo/livrorecomendacao/enums/Genero.java
@@ -0,0 +1,16 @@
+package com.macedo.livrorecomendacao.enums;
+
+public enum Genero {
+
+    FICCAO,
+    ROMANCE,
+    SUSPENSE,
+    LITERATURA,
+    CIENTIFICO,
+    AUTOAJUDA,
+    DIDATICO,
+    GIBIS,
+    POESIA,
+    CONTOS,
+    TERROR
+}
