8f40fe2d8edff62d1557a770c67b8e44d9872864
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-07-11 21:35:45 -0400

    Criado services de editora e livro

---
+++ src/main/java/com/macedo/livrorecomendacao/service/EditoraService.java
@@ -0,0 +1,27 @@
+package com.macedo.livrorecomendacao.service;
+
+import com.macedo.livrorecomendacao.dtos.editoradto.CadastrarEditoraDTO;
+import com.macedo.livrorecomendacao.entity.Editora;
+import com.macedo.livrorecomendacao.repository.EditoraRepository;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EditoraService {
+
+    private final EditoraRepository editoraRepository;
+
+    public EditoraService(EditoraRepository editoraRepository){
+        this.editoraRepository = editoraRepository;
+    }
+
+    public Editora criarEditora(@Valid CadastrarEditoraDTO editoraDTO){
+
+        Editora editora = editoraRepository.findByNome(editoraDTO);
+        if (editora == null){
+            editora = new Editora(editoraDTO);
+            editoraRepository.save(editora);
+        }
+        return editora;
+    };
+}
---
+++ src/main/java/com/macedo/livrorecomendacao/service/LivroService.java
@@ -0,0 +1,37 @@
+package com.macedo.livrorecomendacao.service;
+
+import com.macedo.livrorecomendacao.dtos.editoradto.CadastrarEditoraDTO;
+import com.macedo.livrorecomendacao.dtos.livrodto.CadastrarLivroDTO;
+import com.macedo.livrorecomendacao.entity.Editora;
+import com.macedo.livrorecomendacao.entity.Livro;
+import com.macedo.livrorecomendacao.repository.LivroRespository;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LivroService {
+
+
+    private final LivroRespository livroRespository;
+    private final EditoraService editoraService;
+
+    public LivroService(LivroRespository livroRespository, EditoraService editoraService){
+        this.livroRespository = livroRespository;
+        this.editoraService = editoraService;
+    }
+
+    public void CadastrarLivro(@Valid CadastrarLivroDTO cadastrarLivroDTO){
+
+        CadastrarEditoraDTO cadastrarEditoraDTO = cadastrarLivroDTO.editora();
+        Editora editora = editoraService.criarEditora(cadastrarEditoraDTO);
+
+        Livro livro = new Livro(
+                cadastrarLivroDTO.titulo(),
+                cadastrarLivroDTO.autor(),
+                cadastrarLivroDTO.isbn(),
+                cadastrarLivroDTO.ano(),
+                editora
+        );
+        livroRespository.save(livro);
+    }
+}
