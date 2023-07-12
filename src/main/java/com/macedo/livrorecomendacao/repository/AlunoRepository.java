7e3d31f23473dc8d634bdf32212f082ae7dacfc7
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-07-12 11:30:49 -0400

    Metodo para busca por matricula e livro

--- src/main/java/com/macedo/livrorecomendacao/repository/AlunoRepository.java
+++ src/main/java/com/macedo/livrorecomendacao/repository/AlunoRepository.java
@@ -2,8 +2,11 @@ package com.macedo.livrorecomendacao.repository;
 
 import com.macedo.livrorecomendacao.entity.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+    @Query("SELECT e FROM Aluno e WHERE e.matricula = ?1")
+    Aluno findByMatricula(String matricula);
 }
--- src/main/java/com/macedo/livrorecomendacao/repository/LivroRespository.java
+++ src/main/java/com/macedo/livrorecomendacao/repository/LivroRespository.java
@@ -2,8 +2,11 @@ package com.macedo.livrorecomendacao.repository;
 
 import com.macedo.livrorecomendacao.entity.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LivroRespository extends JpaRepository<Livro, Long> {
+    @Query("SELECT e FROM Livro e WHERE e.isbn = ?1")
+    Livro findByIsbn(String isbn);
 }
