baf38a57925707b1df98c1f3ed395ed1a1cc3934
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-07-11 21:35:17 -0400

    Criado metodo para busca por nome de editora

--- src/main/java/com/macedo/livrorecomendacao/repository/EditoraRepository.java
+++ src/main/java/com/macedo/livrorecomendacao/repository/EditoraRepository.java
@@ -1,9 +1,13 @@
 package com.macedo.livrorecomendacao.repository;
 
+import com.macedo.livrorecomendacao.dtos.editoradto.CadastrarEditoraDTO;
 import com.macedo.livrorecomendacao.entity.Editora;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EditoraRepository extends JpaRepository<Editora, Long> {
+    @Query("SELECT e FROM Editora e WHERE e.nome = ?1")
+    Editora findByNome(CadastrarEditoraDTO editoraDTO);
 }
