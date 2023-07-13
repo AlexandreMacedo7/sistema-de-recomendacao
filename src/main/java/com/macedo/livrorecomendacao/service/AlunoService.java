265d66891d8382d3e548e143e065f24dfd01c72c
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-07-13 17:32:55 -0400

    metodo de listagem de livros avaliados do aluno

--- src/main/java/com/macedo/livrorecomendacao/service/AlunoService.java
+++ src/main/java/com/macedo/livrorecomendacao/service/AlunoService.java
@@ -1,22 +1,30 @@
 package com.macedo.livrorecomendacao.service;
 
 import com.macedo.livrorecomendacao.dtos.alunodto.CadastroAlunoDTO;
+import com.macedo.livrorecomendacao.dtos.alunodto.DadosAlunoDTO;
+import com.macedo.livrorecomendacao.dtos.avaliacao.AvaliacaoDadosDTO;
 import com.macedo.livrorecomendacao.entity.Aluno;
+import com.macedo.livrorecomendacao.entity.Avaliacao;
+import com.macedo.livrorecomendacao.exception.AlunoNaoEncontradoException;
 import com.macedo.livrorecomendacao.repository.AlunoRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AlunoService {
 
     private final AlunoRepository alunoRepository;
 
-    public AlunoService(AlunoRepository alunoRepository){
+    public AlunoService(AlunoRepository alunoRepository) {
         this.alunoRepository = alunoRepository;
     }
 
     @Transactional
-    public void cadastraAluno(CadastroAlunoDTO cadastroAlunoDTO){
+    public void cadastraAluno(CadastroAlunoDTO cadastroAlunoDTO) {
         Aluno aluno = new Aluno(
                 cadastroAlunoDTO.nome(),
                 cadastroAlunoDTO.matricula(),
@@ -29,4 +37,23 @@ public class AlunoService {
         alunoRepository.save(aluno);
     }
 
+    public DadosAlunoDTO listarDadosAluno(String matricula) {
+
+        try {
+            Aluno aluno = alunoRepository.findByMatricula(matricula);
+
+            if (aluno == null) {
+                throw new AlunoNaoEncontradoException("Matricula: " + matricula + "Não encontrada!");
+            }
+
+            List<AvaliacaoDadosDTO> avaliacoesDTOS = new ArrayList<>();
+
+            List<AvaliacaoDadosDTO> avaliacaoDadosDTOS = aluno.getAvaliacaoLista().stream().map(avaliacao -> new AvaliacaoDadosDTO(
+                    avaliacao.getLivro().getIsbn(),avaliacao.getNota())).collect(Collectors.toList());
+            return new DadosAlunoDTO(aluno.getNome(), aluno.getEmail(),
+                    aluno.getTurma(), aluno.getTurno(), avaliacoesDTOS);
+        } catch (Exception ex){
+            throw new RuntimeException("Erro ao buscar dados do aluno " + ex.getMessage());
+        }
+    }
 }
