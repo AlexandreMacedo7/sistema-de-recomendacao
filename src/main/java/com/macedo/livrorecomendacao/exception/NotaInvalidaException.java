d0c2766e5cac0a0c18c54e047de7642c7b7b603e
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-07-19 22:22:05 -0400

    Criada exceção personalizada para Nota invalida

---
+++ src/main/java/com/macedo/livrorecomendacao/exception/NotaInvalidaException.java
@@ -0,0 +1,8 @@
+package com.macedo.livrorecomendacao.exception;
+
+public class NotaInvalidaException extends RuntimeException{
+    public NotaInvalidaException(String message){
+        super(message);
+    }
+
+}
