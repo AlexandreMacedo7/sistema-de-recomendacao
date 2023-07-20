6e9b6de0aae019117f31537b80c7c7e210514164
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-07-19 21:39:41 -0400

    Criação de controller para listagem de avaliações por meio de paginação

--- src/main/java/com/macedo/livrorecomendacao/controller/AvaliacaoController.java
+++ src/main/java/com/macedo/livrorecomendacao/controller/AvaliacaoController.java
@@ -1,12 +1,14 @@
 package com.macedo.livrorecomendacao.controller;
 
+import com.macedo.livrorecomendacao.dtos.avaliacaodto.AvaliacaoDadosGeralDTO;
 import com.macedo.livrorecomendacao.dtos.avaliacaodto.CadastrarAvaliacaoDTO;
 import com.macedo.livrorecomendacao.service.AvaliacaoService;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/avaliacao")
@@ -22,5 +24,10 @@ public class AvaliacaoController {
     @PostMapping
     public void cadastrarAvaliacao(@RequestBody @Valid CadastrarAvaliacaoDTO avaliacaoDTO){
         avaliacaoService.cadastrarAvaliacao(avaliacaoDTO);
+    }
+    @GetMapping
+    public ResponseEntity<Page<AvaliacaoDadosGeralDTO>> listarAvaliacoes(@PageableDefault(size = 10, sort = "aluno")Pageable pageable){
+        Page<AvaliacaoDadosGeralDTO> avaliacaoDadosDTOS = avaliacaoService.listarAvaliacoes(pageable);
+        return ResponseEntity.ok(avaliacaoDadosDTOS);
     }
 }
