66cbd9af09e08191a53dd4fcdb5c4d5ebfe920c5
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-07-19 21:37:08 -0400

    Mudança de atributo nome para titulo

--- src/main/java/com/macedo/livrorecomendacao/dtos/avaliacaodto/AvaliacaoDadosDTO.java
+++ src/main/java/com/macedo/livrorecomendacao/dtos/avaliacaodto/AvaliacaoDadosDTO.java
@@ -1,12 +1,12 @@
 package com.macedo.livrorecomendacao.dtos.avaliacaodto;
 
 public record AvaliacaoDadosDTO(
-        String nome,
+        String titulo,
         String isbn,
         double nota
 ) {
-    public AvaliacaoDadosDTO(String nome, String isbn, double nota) {
-        this.nome = nome;
+    public AvaliacaoDadosDTO(String titulo, String isbn, double nota) {
+        this.titulo = titulo;
         this.isbn = isbn;
         this.nota = nota;
     }
