006beb6eee2d4f5f36573c45c16ef103ce67cad7
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-07-21 15:54:02 -0400

    Criado Query especifica para busca e deleção de avaliação por matricula e isbn

--- src/main/java/com/macedo/livrorecomendacao/repository/AvaliacaoRepository.java
+++ src/main/java/com/macedo/livrorecomendacao/repository/AvaliacaoRepository.java
@@ -1,9 +1,16 @@
 package com.macedo.livrorecomendacao.repository;
 
 import com.macedo.livrorecomendacao.entity.Avaliacao;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Avaliacao a WHERE a.aluno.matricula = :matricula AND a.livro.isbn = :isbn")
+    void deleteByMatriculaEIsbn(String matricula, String isbn);
 }
