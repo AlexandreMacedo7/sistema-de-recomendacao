eb1466ad0cc85e021872a625fc5453c44e781e03
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-08-03 14:30:41 -0400

    Metodo modificado para retorno de codigo 201 e retorno de valores com dto

--- src/main/java/com/macedo/livrorecomendacao/controller/AlunoController.java
+++ src/main/java/com/macedo/livrorecomendacao/controller/AlunoController.java
@@ -12,34 +12,43 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/aluno")
 public class AlunoController {
 
     private final AlunoService alunoService;
-    public AlunoController(AlunoService alunoService){
+
+    public AlunoController(AlunoService alunoService) {
         this.alunoService = alunoService;
     }
 
     @PostMapping
-    public void cadastrarAluno(@RequestBody @Valid CadastroAlunoDTO dados){
-        alunoService.cadastraAluno(dados);
+    @Transactional
+    public ResponseEntity cadastrarAluno(@RequestBody @Valid CadastroAlunoDTO dados, UriComponentsBuilder uriBuilder) {
+        var aluno = alunoService.cadastraAluno(dados);
+
+        var uri = uriBuilder.path("/aluno/matricula").buildAndExpand(aluno.getMatricula()).toUri();
+
+        return ResponseEntity.created(uri).body(new CadastroAlunoDTO(aluno));
     }
 
     @PostMapping("/pesquisa")
-    public DadosAlunoIndividualDTO dadosAlunoDTO(@RequestBody @Valid PesquisaMatriculaDTO matriculaAluno){
+    public DadosAlunoIndividualDTO dadosAlunoDTO(@RequestBody @Valid PesquisaMatriculaDTO matriculaAluno) {
         DadosAlunoIndividualDTO alunoDTO;
         return alunoDTO = alunoService.listarDadosAluno(matriculaAluno.matricula());
     }
+
     @GetMapping
-    public ResponseEntity<Page<AlunoListagemDTO>> listarAlunos(@PageableDefault(size = 10, sort ={"nome"}) Pageable pageable){
-		var page = alunoService.listarAlunos(pageable);
+    public ResponseEntity<Page<AlunoListagemDTO>> listarAlunos(@PageableDefault(size = 10, sort = {"nome"}) Pageable pageable) {
+        var page = alunoService.listarAlunos(pageable);
         return ResponseEntity.ok(page);
     }
+
     @DeleteMapping
     @Transactional
-    public ResponseEntity excluirAlunoPorMatricula(@RequestBody PesquisaMatriculaDTO matriculaDTO){
+    public ResponseEntity excluirAlunoPorMatricula(@RequestBody PesquisaMatriculaDTO matriculaDTO) {
         alunoService.deletarAluno(matriculaDTO.matricula());
         return ResponseEntity.noContent().build();
     }
